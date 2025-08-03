4523a8c09007b52386e7105e7ca85721341fefa3
kik_lenovo_17 <kik87827@gmail.com>
2025-08-03 20:14:32 +0900

    진행중2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import bgimg from './img/bg-1.png';
 
 function App() {
   return (
@@ -16,6 +17,28 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <div className="main-bg" style={{backgroundImage : `url(${bgimg})`}}></div>
+      <div>
+        <Container>
+          <Row>
+            <Col xs={12} md={4}>
+              <img src={process.env.PUBLIC_URL + '/shoes1.jpg'} width="80%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col xs={12} md={4}>
+              <img src={process.env.PUBLIC_URL + '/shoes2.jpg'} width="80%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col xs={12} md={4}>
+              <img src={process.env.PUBLIC_URL + '/shoes3.jpg'} width="80%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
