0503c704c59e4225fced45cd7545f4f03439edc1
kik8727 <kik87827@gmail.com>
2025-08-09 15:58:40 +0900

    진행중8

--- src/App.js
+++ src/App.js
@@ -8,15 +8,35 @@ import { Routes, Route, useNavigate} from 'react-router-dom';
 import DetailPage from './pages/DetailPage';
 import AboutPage from './pages/AboutPage';
 import EventPage from './pages/EventPage';
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+  background : ${ props => props.bg || 'yellow' };
+  color: ${ props => props.bg == 'green' ? '#fff' : '#000'};
+  padding: 10px;
+`
+
+let NewBtn = styled(YellowBtn)`
+  border : 2px solid red;
+`;
+
+let Box = styled.div`
+  background : grey;
+  padding : 20px;
+`
 
 function App() {
+  let navigate = useNavigate();
 
   let [shoes,setShoes] = useState(shoesData);
-  let navigate = useNavigate();
 
   return (
     <div className="App">
-      
+      <Box>
+        <YellowBtn>버튼</YellowBtn>
+        <YellowBtn bg="blue">버튼</YellowBtn>
+        <NewBtn bg="green">버튼</NewBtn>
+      </Box>
       {/* <Button variant="primary">Primary</Button> */}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
@@ -48,9 +68,9 @@ function App() {
               <Container>
                 <Row>
                   {
-                    shoes.map((item)=>{
+                    shoes.map((item,index)=>{
                       return (
-                        <ShoesCard senditem={item} key={item.id}  onClick={()=>{navigate('/detail/'+item.id)}} />
+                        <ShoesCard senditem={item} key={item.id} clickIndex={index} navigate={navigate} />
                       )
                     })
                   }
@@ -75,13 +95,17 @@ function App() {
   );
 }
 
-function ShoesCard({senditem}){
+function ShoesCard({ senditem, clickIndex, navigate  }) {
   return(
     <Col xs={12} md={4}>
-      <img src={process.env.PUBLIC_URL + `/shoes${senditem.id+1}.jpg`} width="80%" alt="" />
-      <h4>{senditem.title}</h4>
-      <p>{senditem.content}</p>
-      <p>{senditem.price}원</p>
+      <div onClick={() => {
+        navigate('/detail/' + clickIndex);
+      }}>
+        <img src={process.env.PUBLIC_URL + `/shoes${senditem.id+1}.jpg`} width="80%" alt="" />
+        <h4>{senditem.title}</h4>
+        <p>{senditem.content}</p>
+        <p>{senditem.price}원</p>
+      </div>
     </Col>
   )
 }
